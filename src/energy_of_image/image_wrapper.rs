eb8553d9e2b811624915cc81f07bb7e408767815
Jasper Sarrazin <Jasper.Sarrazin@tomtom.com>
2022-05-24 08:28:32 +0200

    energy grid + docs

--- src/energy_of_image/image_wrapper.rs
+++ src/energy_of_image/image_wrapper.rs
@@ -1,13 +1,16 @@
+use std::collections::HashMap;
 use image::DynamicImage;
 use image::io::Reader as ImageReader;
 
-use crate::energy_of_image::energy::get_energy_image;
+use crate::energy_of_image::energy::{get_energy_grid, get_energy_image};
 
 pub struct ImageWrapper {
     original: Option<DynamicImage>,
 }
 
 impl ImageWrapper {
+
+    /// loads the wrapper with an image stored at the path argument
     pub fn from_file(path: &str) -> Self {
         let result = ImageReader::open(path);
         match result {
@@ -30,13 +33,36 @@ impl ImageWrapper {
         }
     }
 
-    pub fn get_energy(&self) -> DynamicImage {
+    /// get the energy grid
+    pub fn get_energy(&self) -> HashMap<(u32,u32), u32> {
+        match &self.original {
+            Some(image) => {
+                get_energy_grid(image)
+            },
+            None =>{
+                panic!("No image was loaded to calculate energy")
+            }
+        }
+    }
+
+    /// get the energy image
+    pub fn get_energy_image(&self) -> DynamicImage {
         match &self.original {
             Some(image) => {
                 get_energy_image(image)
-            }
+            },
             None => {
                 panic!("No image was loaded to calculate energy")
+            }
+        }
+    }
+
+    /// get the image out of the wrapper
+    pub fn get_image(&self) -> &DynamicImage {
+        match &self.original {
+            Some(image) => image,
+            None => {
+                panic!("No image was loaded yet!")
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod energy_of_image;
 
 fn main() {
     let image = ImageWrapper::from_file("./images/surfer.jpg");
-    let energy = image.get_energy();
+    let energy = image.get_energy_image();
     let image_result = energy.save("./images/energy.jpg");
     match image_result {
         Ok(_) => {}
